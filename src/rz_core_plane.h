d160e11e10b8dfc2421be3da70354d0d64ebfb35
Conrad Rozetti <crz@6itproject.org>
2017-04-14 11:20:36 +0100

    crz: created accessors for rz::plane members

--- src/rz_core_plane.h
+++ src/rz_core_plane.h
@@ -19,6 +19,11 @@ namespace rz
 		rz::vector3 normal;
 		float distance_from_origin;
 
+		inline float x() const { return normal.x(); }
+		inline float y() const { return normal.y(); }
+		inline float z() const { return normal.z(); }
+		inline float d() const { return distance_from_origin; }
+
 		rz::plane(rz::vector3 const &normal, float distance);
 		rz::plane(float x, float y, float z, float d);
 		
